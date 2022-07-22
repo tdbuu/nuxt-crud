e13ec4e409897f0847cbc79295ff8e9a82a525ac
btngoc@cmcglobal.vn <btngoc@cmcglobal.vn>
2022-07-22 15:35:41 +0700

    fix bug:
    - fix type mismatch in customer.services.ts
    - generate a sample page in index.vue (not done)

---
+++ components/CustomerList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Customer List</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CustomerList",
+    data() {
+        return {
+            customerList: [],
+            count: 0
+        }
+    },
+    mounted() {
+        console.log(this);
+    }
+} 
+</script>
